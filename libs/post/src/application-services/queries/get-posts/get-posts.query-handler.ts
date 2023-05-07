882205af5ad1035d68c57ad6717c6081742948c0
Alexey Andryushin <andr200116alex@gmail.com>
2023-05-07 15:32:18 +0300

    feat: update facade, make a repository

--- libs/post/src/application-services/queries/get-posts/get-posts.query-handler.ts
+++ libs/post/src/application-services/queries/get-posts/get-posts.query-handler.ts
@@ -6,20 +6,20 @@ import { Logger } from '@nestjs/common';
 
 @QueryHandler(GetPostsQuery)
 export class GetPostsQueryHandler
-  implements IQueryHandler<GetPostsQuery, [[PostAggregate], number]>
+  implements IQueryHandler<GetPostsQuery, [PostAggregate[], number]>
 {
   constructor(private readonly postRepository: PostRepository) {}
   private readonly logger = new Logger(GetPostsQueryHandler.name);
 
   async execute({
     pagination,
-  }: GetPostsQuery): Promise<[[PostAggregate], number]> {
+  }: GetPostsQuery): Promise<[PostAggregate[], number]> {
     const [data, count] = await this.postRepository
       .findAll(pagination)
       .catch((err) => {
         this.logger.error(err);
         return [[], 0];
       });
-    return [data, count] as [[PostAggregate], number];
+    return [data, count] as [PostAggregate[], number];
   }
 }
