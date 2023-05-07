52605eba5dcf1aa1f5f3e537aea3bc3375bb7109
Alexey Andryushin <andr200116alex@gmail.com>
2023-05-07 16:26:53 +0300

    feat: add auth & update post controller

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { PostFacade } from '@lib/post/application-services';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePostDto } from './dto';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postFacade: PostFacade) {}
+
+  @Post()
+  createPost(@Body() createPostDto: CreatePostDto) {
+    return this.postFacade.commands.createPost(createPostDto);
+  }
+}
