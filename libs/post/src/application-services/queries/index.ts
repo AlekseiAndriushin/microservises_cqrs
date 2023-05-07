2932a3f820f33279e8fcd27a76221a8787f2f7f4
Alexey Andryushin <andr200116alex@gmail.com>
2023-05-07 13:13:52 +0300

    chore: add import

--- libs/post/src/application-services/queries/index.ts
+++ libs/post/src/application-services/queries/index.ts
@@ -1,7 +1,9 @@
 import { Type } from '@nestjs/common';
 import { IQueryHandler } from '@nestjs/cqrs';
 import { GetPostQueryHanlder } from './get-post/get-post.query-handler';
+import { GetPostsQueryHandler } from './get-posts/get-posts.query-handler';
 
 export const POST_QUERIES_HANDLERS: Type<IQueryHandler>[] = [
   GetPostQueryHanlder,
+  GetPostsQueryHandler,
 ];
